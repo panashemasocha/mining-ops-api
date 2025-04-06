5515046be58c59a383122c5794d1fbeccc600a11
panashe Nash masocha <nashmasocha@outlook.com>
2025-04-06 09:17:39 +0200

    Added global exceptions on the api exceptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,13 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -49,26 +54,21 @@ class Handler extends ExceptionHandler
                 'message' => 'The requested resource was not found',
             ], 404);
         });
-        
 
         $this->reportable(function (Throwable $e) {
             //
         });
-
-
     }
-
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param \Illuminate\Http\Request  $request
-     * @param \Throwable  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable $exception
      * @return \Illuminate\Http\Response
      */
     public function render($request, Throwable $exception)
     {
-
         if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
             return response()->json([
                 'error' => 'Resource Not Found',
@@ -76,6 +76,24 @@ class Handler extends ExceptionHandler
             ], 404);
         }
 
+        if ($exception instanceof AuthenticationException && $request->expectsJson()) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        }
+
+        if ($exception instanceof AuthorizationException && $request->expectsJson()) {
+            return response()->json(['message' => 'Unauthorized.'], 403);
+        }
+
+        if ($exception instanceof ValidationException && $request->expectsJson()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $exception->errors(),
+            ], 422);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException && $request->expectsJson()) {
+            return response()->json(['message' => 'Method Not Allowed'], 405);
+        }
 
         return parent::render($request, $exception);
     }
