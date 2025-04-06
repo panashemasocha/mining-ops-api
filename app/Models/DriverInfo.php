75bc8190726996fc2784fbd092715cdcdb48d6de
panashe Nash masocha <nashmasocha@outlook.com>
2025-04-06 10:39:00 +0200

    explicitly defined the table name for driver_info

--- app/Models/DriverInfo.php
+++ app/Models/DriverInfo.php
@@ -8,11 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class DriverInfo extends Model
 {
     use HasFactory;
+    protected $table = 'driver_info';
+
     protected $fillable = [
         'user_id', 'license_number', 'last_known_longitude', 'last_known_latitude',
         'last_known_altitude', 'status'
     ];
-    
+
 
     public function user()
     {
