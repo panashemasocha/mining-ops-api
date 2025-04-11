961f500ae24fc5ff2a6966c1ba65c6cd6fbcc2d8
panashe Nash masocha <nashmasocha@outlook.com>
2025-04-11 14:39:09 +0200

    Added reference to consolidated controller on routes/api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\AssignedVehicleController;
+use App\Http\Controllers\V1\ConsolidatedDataController;
 use App\Http\Controllers\V1\CostPriceController;
 use App\Http\Controllers\V1\DispatchController;
 use App\Http\Controllers\V1\JobPositionController;
