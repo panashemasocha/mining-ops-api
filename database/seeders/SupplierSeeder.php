022b66b77a68cb4e68c381d5273e2f9b2c1f0449
panashe Nash masocha <nashmasocha@outlook.com>
2025-04-18 21:55:55 +0200

    Updated suppliers seeder to use Zim names

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -24,15 +24,47 @@ class SupplierSeeder extends Seeder
             $query->where('name', 'management');
         })->get();
 
+        $zimbabweanFirstNames = [
+            'Tawanda',
+            'Tinashe',
+            'Chiedza',
+            'Rudo',
+            'Farai',
+            'Tendai',
+            'Munyaradzi',
+            'Fungai',
+            'Tariro',
+            'Nokutenda',
+            'Tapiwa',
+            'Anesu',
+            'Rumbidzai'
+        ];
+
+        $zimbabweanLastNames = [
+            'Moyo',
+            'Ncube',
+            'Chikore',
+            'Khumalo',
+            'Sithole',
+            'Dube',
+            'Mhlanga',
+            'Nyathi',
+            'Mandaza',
+            'Mangena',
+            'Zondo',
+            'Mupindu',
+            'Mpofu'
+        ];
+
         for ($i = 0; $i < 5; $i++) {
             Supplier::create([
-                'first_name' => $this->faker->firstName,
-                'last_name' => $this->faker->lastName,
-                'national_id' => '63-' . $this->faker->unique()->numberBetween(1000000, 9999999) . '-A00',
-                'physical_address' => $this->faker->address,
-                'created_by' => $users->random()->id,
-                'payment_method_id' => $paymentMethods->random()->id,
-                'phone_number' => '+263' . $this->faker->unique()->numberBetween(712000000, 779999999),
+                'first_name'       => $this->faker->randomElement($zimbabweanFirstNames),
+                'last_name'        => $this->faker->randomElement($zimbabweanLastNames),
+                'national_id'      => '63-' . $this->faker->unique()->numberBetween(1000000, 9999999) . '-A00',
+                'physical_address' => $this->faker->streetAddress . ', ' . $this->faker->randomElement(['Karoi', 'Harare']),
+                'created_by'       => $users->random()->id,
+                'payment_method_id'=> $paymentMethods->random()->id,
+                'phone_number'     => '+263' . $this->faker->unique()->numberBetween(712000000, 779999999),
             ]);
         }
     }
