7e56c903f4c8407d78fb90aac4285c44b2a4f18e
panashe Nash masocha <nashmasocha@outlook.com>
2025-05-05 13:40:41 +0200

    Updated trip authorization and update method 2

--- app/Http/Requests/UpdateTripRequest.php
+++ app/Http/Requests/UpdateTripRequest.php
@@ -17,35 +17,46 @@ class UpdateTripRequest extends FormRequest
         $prefix = $this->isMethod('put') ? 'required|' : 'sometimes|';
 
         return [
-            'driver_id'           => "{$prefix}exists:users,id",
-            'vehicle_id'          => "{$prefix}exists:vehicles,id",
-            'dispatch_id'         => "{$prefix}exists:dispatches,id",
-            'ore_quantity'        => "{$prefix}numeric|min:0",
-            'initial_longitude'   => "{$prefix}nullable|numeric|between:25.237,33.056",
-            'initial_latitude'    => "{$prefix}nullable|numeric|between:-22.421,-15.609",
-            'initial_altitude'    => "{$prefix}nullable|numeric|min:0|max:2000",
-            'final_longitude'     => "{$prefix}numeric|between:25.237,33.056",
-            'final_latitude'      => "{$prefix}numeric|between:-22.421,-15.609",
-            'final_altitude'      => "{$prefix}numeric|min:0|max:2000",
-            'diesel_allocation_id'=> "{$prefix}nullable|exists:diesel_allocations,id",
-            'status'              => "{$prefix}in:fulfilled,pending,in-transit,failed",
+            'driver_id' => "{$prefix}exists:users,id",
+            'vehicle_id' => "{$prefix}exists:vehicles,id",
+            'dispatch_id' => "{$prefix}exists:dispatches,id",
+            'ore_quantity' => "{$prefix}numeric|min:0",
+            'initial_longitude' => "{$prefix}nullable|numeric|between:25.237,33.056",
+            'initial_latitude' => "{$prefix}nullable|numeric|between:-22.421,-15.609",
+            'initial_altitude' => "{$prefix}nullable|numeric|min:0|max:2000",
+            'final_longitude' => "{$prefix}numeric|between:25.237,33.056",
+            'final_latitude' => "{$prefix}numeric|between:-22.421,-15.609",
+            'final_altitude' => "{$prefix}numeric|min:0|max:2000",
+            'diesel_allocation_id' => "{$prefix}nullable|exists:diesel_allocations,id",
+            'status' => "{$prefix}in:fulfilled,pending,in-transit,failed",
         ];
     }
 
     protected function prepareForValidation()
     {
-        $this->merge([
-            'driver_id'            => $this->input('driver_id', $this->input('driverId')),
-            'vehicle_id'           => $this->input('vehicle_id', $this->input('vehicleId')),
-            'dispatch_id'          => $this->input('dispatch_id', $this->input('dispatchId')),
-            'ore_quantity'         => $this->input('ore_quantity', $this->input('oreQuantity')),
-            'initial_longitude'    => $this->input('initial_longitude', $this->input('initialLongitude')),
-            'initial_latitude'     => $this->input('initial_latitude', $this->input('initialLatitude')),
-            'initial_altitude'     => $this->input('initial_altitude', $this->input('initialAltitude')),
-            'final_longitude'      => $this->input('final_longitude', $this->input('finalLongitude')),
-            'final_latitude'       => $this->input('final_latitude', $this->input('finalLatitude')),
-            'final_altitude'       => $this->input('final_altitude', $this->input('finalAltitude')),
-            'diesel_allocation_id' => $this->input('diesel_allocation_id', $this->input('dieselAllocationId')),
-        ]);
+        $mapping = [
+            'driver_id' => ['driver_id', 'driverId'],
+            'vehicle_id' => ['vehicle_id', 'vehicleId'],
+            'dispatch_id' => ['dispatch_id', 'dispatchId'],
+            'ore_quantity' => ['ore_quantity', 'oreQuantity'],
+            'initial_longitude' => ['initial_longitude', 'initialLongitude'],
+            'initial_latitude' => ['initial_latitude', 'initialLatitude'],
+            'initial_altitude' => ['initial_altitude', 'initialAltitude'],
+            'final_longitude' => ['final_longitude', 'finalLongitude'],
+            'final_latitude' => ['final_latitude', 'finalLatitude'],
+            'final_altitude' => ['final_altitude', 'finalAltitude'],
+            'diesel_allocation_id' => ['diesel_allocation_id', 'dieselAllocationId'],
+            'status' => ['status', 'status'],
+        ];
+
+        $data = [];
+        foreach ($mapping as $snake => [$snakeKey, $camelKey]) {
+            if ($this->has($snakeKey) || $this->has($camelKey)) {
+                $data[$snake] = $this->input($snakeKey, $this->input($camelKey));
+            }
+        }
+
+        $this->merge($data);
     }
+
 }
