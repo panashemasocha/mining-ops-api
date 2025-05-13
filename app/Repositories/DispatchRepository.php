23e5b148767f5079f0c0780b16351b73a282375e
panashe Nash masocha <nashmasocha@outlook.com>
2025-05-13 04:00:34 +0200

    Updated dispatch for driver by removiing date filters around it

--- app/Repositories/DispatchRepository.php
+++ app/Repositories/DispatchRepository.php
@@ -36,8 +36,8 @@ class DispatchRepository
         return Dispatch::whereHas('trips', function ($query) use ($driverId) {
             $query->where('driver_id', $driverId);
         })
-            ->whereDate('created_at', '>=', $startDate)
-            ->whereDate('created_at', '<=', $endDate)
+            // ->whereDate('created_at', '>=', $startDate)
+            // ->whereDate('created_at', '<=', $endDate)
             ->orderBy('created_at', 'desc')
             ->get();
     }
