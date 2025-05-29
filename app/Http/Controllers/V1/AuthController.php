9b902b36a52a78b7204c2b51603d3c16eb2dade8
panashe Nash masocha <nashmasocha@outlook.com>
2025-05-29 13:33:17 +0200

    Updated auth message on logging out

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->user()->tokens()->delete();
-        return response()->json(['message' => 'Logged out']);
+        return response()->json(['info' => 'Logged out']);
     }
 
     /**
